2b85fd8aa6a43c479a7b69b1bd12b9c24a33c424
muyi <38781672@qq.com>
2022-08-18 22:56:33 +0800

    feat：脚本更换

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 打包生成静态文件
+yarn run docs:build
+
+# 进入生成的文件夹
+# cd docs/.vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+# git init
+# git add -A
+# git commit -m "Git Action 自动部署"
+
+# # 如果发布到 https://<USERNAME>.github.io   -f 强制推送
+# git push -f git@github.com:xinlei3166/vitepress-demo.git master:gh-pages
