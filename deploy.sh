8393f124456c9fa83022a799cdbbc79db4709275
muyi <38781672@qq.com>
2022-08-21 22:58:33 +0800

    chore：脚本修改

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,14 @@ yarn run docs:build
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
+# time=$(date "+%Y-%m-%d %H:%M:%S") 
+
 # git init
 # git add -A
-# git commit -m "Git Action 自动部署"
+# git commit -m "GitHub Action 自动部署：$time"
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # # 如果发布到 https://<USERNAME>.github.io   -f 强制推送
-# git push -f git@github.com:xinlei3166/vitepress-demo.git master:gh-pages
+# git push -f git@github.com:muyaCode/FrontEndLearningNotes.git master:gh-pages
