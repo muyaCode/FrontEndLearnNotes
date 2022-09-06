ad381b05b2000327ccb144b704997243bd479f8f
hujiexin <38781672@qq.com>
2022-09-06 08:47:11 +0800

    fix：修正github地址链接

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -44,7 +44,7 @@ export default {
       // { icon: 'slack', link: 'https://github.com/' },
       // { icon: 'twitter', link: 'https://github.com/' },
       // { icon: 'youtube', link: 'https://github.com/' },
-      { icon: 'github', link: 'https://github.com/muyaCode/FrontEndLearningNotes' },
+      { icon: 'github', link: 'https://github.com/muyaCode/FrontEndLearnNotes' },
       {
         icon: {
           svg: `<svg t="1660806015529" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1999" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200">
