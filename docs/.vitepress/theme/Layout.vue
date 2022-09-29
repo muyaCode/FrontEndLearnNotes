415046002af6a359bc8cb439701ecc5e8a2a08e1
hujiexin <38781672@qq.com>
2022-09-29 10:06:26 +0800

    chore：添加主题

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import DefaultTheme from 'vitepress/theme'
+  
+  const { Layout } = DefaultTheme
+  </script>
+  
+  <template>
+    <Layout>
+      <template #aside-outline-before>
+        My custom sidebar top content
+      </template>
+    </Layout>
+  </template>
