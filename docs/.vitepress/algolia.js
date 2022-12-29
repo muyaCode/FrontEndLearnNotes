c56c7c40af5b89b10aa68ba3ce57090a778814e5
muyi <38781672@qq.com>
2022-12-29 22:16:41 +0800

    chore：algolia搜索配置

--- docs/.vitepress/algolia.js
+++ docs/.vitepress/algolia.js
@@ -1,8 +1,8 @@
 // 展示 algolia 搜索框
 export default {
-  appId: '',
-  apiKey: '',
-  indexName: '',
+  appId: '1L6FRXPKEM',
+  apiKey: '934b4c2e67a08d72d379c9af236c5859',
+  indexName: 'FrontEndLearnNotes',
   placeholder: '请输入关键词',
   buttonText: '搜索',
   searchParameters: {
