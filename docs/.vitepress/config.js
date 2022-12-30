41004259fb3b7fb58b762186ae2483f4d639f13e
muyi <38781672@qq.com>
2022-12-31 00:19:27 +0800

    feat：更新配置注释

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -60,15 +60,17 @@ export default {
       text: '在 GitHub 上编辑此页'
     },
     lastUpdateText: '上次更新时间：',
-    // 卡片广告
-    carbonAds: {
-      code: '卡片广告 code',
-      placement: '卡片广告布置'
-    },
     docFooter: {
       prev: '上一篇',
       next: '下一篇'
     },
+
+    // 右边文档大纲下面的-卡片广告
+    // carbonAds: {
+    //   code: '卡片广告 code',
+    //   placement: '卡片广告布置'
+    // },
+    
     // 首页页脚配置。您可以添加消息和版权。仅当页面由于设计原因不包含边栏时，才会显示页脚。
     footer: {
       message: 'Released under the MIT License.',
